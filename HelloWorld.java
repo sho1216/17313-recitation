d070c95e87fe2a7332ed4f54e87efc5e8232f93b
nikithamurikinati <nikitham99@gmail.com>
2020-09-04 15:56:06 -0400

    added hi

--- HelloWorld.java
+++ HelloWorld.java
@@ -22,6 +22,7 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
 
         System.out.println("Hello, World, Sandra Ho 3");
+	System.out.println("Niki says hi");
     }
 
 }
